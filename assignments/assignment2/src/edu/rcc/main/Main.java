15668bea52ed84bb0fd777815e014d784233ab5f
Nicholas Rhodes <rhodes.corp@gmail.com>
2014-09-25 20:44:49 -0700

    commit

---
+++ assignments/assignment2/src/edu/rcc/main/Main.java
@@ -0,0 +1,14 @@
+package edu.rcc.main;
+
+import edu.rcc.deck.Card;
+import edu.rcc.deck.CardDeck;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Card c = new Card("Diamond", "2");
+		CardDeck d = CardDeck.getStandardDeck();
+		
+	}
+
+}
--- lectures/lecture4/src/edu/rcc/Main.java
+++ lectures/lecture4/src/edu/rcc/Main.java
@@ -8,6 +8,9 @@ public class Main {
 		agentX.walks(max);
 		agentX.walks(max);
 		Cat superMax = new UberCat();
+		if (superMax instanceof UberCat) {
+			UberCat uberVersion = (UberCat)superMax;
+		}
 		for (int i = 0; i < 6; ++i) {
 			agentX.walks(superMax);
 		}
