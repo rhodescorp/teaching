7e305dbde4656aa36e8b5b9a9ef6af3bdeeb7a99
Nicholas Rhodes <nrhodes@esri.com>
2014-10-17 13:23:04 -0700

    Update Parsing.java

--- lectures/lecture7/src/edu/rcc/parse/Parsing.java
+++ lectures/lecture7/src/edu/rcc/parse/Parsing.java
@@ -3,8 +3,26 @@ package edu.rcc.parse;
 public class Parsing {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		MyStack stack = new MyStack();
+		String parensString = args[0];
+		for (int i = 0; i < parensString.length(); ++i) {
+			Character c = parensString.charAt(i);
+			if (c == '(') {
+				stack.push(c);
+			} else {
+				if (stack.peek() == '(') {
+					stack.pop();
+				} else {
+					System.out.println("error too many close parens");
+				}
+			}
+		}
+		if (stack.peek() == '(') {
+			System.out.println("Error too many open parens);
+		}
+		
+		String sentence = "the quick brown fox jumped over the lazy dog";
+		String[] wordsOfSentence = sentence.split(" ");
 	}
 
 }
